5f4fa61ef7367c5e0787cc58063e64ca5f591ef9
Nyo777BR <95981787+Nyo777BR@users.noreply.github.com>
2024-11-30 17:50:16 -0300

    Finalizando - Donos dos Restaurantes
    Basicamente, a parte dos donos dos restaurantes está finalizada.

--- projetofinal_donos.c
+++ projetofinal_donos.c
@@ -12,7 +12,7 @@ int numPratos = 0;
 #define MAX_STRING_LENGTH 50
 
 #define restsTxt "restaurantes.txt"
-#define pratosTxt "pratosTxt"
+#define pratosTxt "pratos.txt"
 #define pedidosTxt "pedidos.txt"
 #define clientesTxt "clientes.txt"
 
@@ -36,10 +36,17 @@ void allocerror();
 void removeNewLineFromStrEnd(char* str);
 
 int findIndexOfRestByCode(int codigoRestaurante);
+int findIndexOfPratoByItsCodeAndRestCode(int pratoCode, int restCode);
 
+int deleteRest(int restIndex);
 int createNewRest();
 int saveRests();
 int readRests();
+
+int deletePrato(int pratoIndex);
+int createNewPrato();
+int savePratos();
+int readPratos();
 
 //Função para cabeçalho
 void mostrarCabecalho() {
@@ -111,15 +118,11 @@ void mostrarPratos(int codigoRest) {
 
     //Definindo NUM_PRATOS_POR_RESTAURANTE temporariamente
     //para evitar erros de compilação
-    int NUM_PRATOS_POR_RESTAURANTE = 0;
 
     printf("\nPratos disponiveis no restaurante %s:\n", restaurantes[restIndex].nome);
-    for (int j = 0; j < NUM_PRATOS_POR_RESTAURANTE; j++) {
-        
-        int pratoIndex = restIndex * NUM_PRATOS_POR_RESTAURANTE + j;
-        Prato prato = pratos[pratoIndex];
-        if (prato.codigoPrato != -1){
-            printf("%d - %s - R$%.2f\n", prato.codigoPrato, prato.descricao, prato.preco);
+    for (int j = 0; j < numPratos; j++) {
+        if (pratos[j].codigoRest == codigoRest) {
+            printf(" %i - %s - R$%.2f\n", pratos[j].codigoPrato, pratos[j].descricao, pratos[j].preco);
         }
     }
 }
@@ -143,13 +146,15 @@ void renomearRestaurante(Restaurante *restaurante) {
 void renomearPrato (Prato *prato){
     char novo_nome[MAX_STRING_LENGTH];
     getchar();
-    printf ("%s - Digite o novo nome: ", prato->descricao);
+    printf ("%s - Digite o novo nome:\n", prato->descricao);
     fgets(novo_nome, MAX_STRING_LENGTH, stdin);
     removeNewLineFromStrEnd(novo_nome); //remove o \n após o final do fgets
 
     strcpy(prato->descricao, novo_nome);
 
     printf("O novo nome eh: %s\n", prato->descricao);
+
+    savePratos();
 }
 
 //função da tela de login e menu de opcoes
@@ -174,15 +179,15 @@ void login() {
         printf("\nLOGIN UAIFOOD RESTAURANTE\n");
         printf("\nDigite o codigo do restaurante (ou um numero negativo para sair):\n");
         scanf("%d", &codigoRestaurante);
-        index_restaurante = findIndexOfRestByCode(codigoRestaurante);
-
+        
         if (codigoRestaurante < 0) {
             break; //Sair 
         }
-        //Ver se o restaurante existe
-        int i = findIndexOfRestByCode(codigoRestaurante);
 
-        if (i == -1) {
+        //Ver se o restaurante existe
+        int restIndex = findIndexOfRestByCode(codigoRestaurante);
+
+        if (restIndex == -1) {
             printf("\nRestaurante nao encontrado. Tente novamente.\n");
             continue; //volta para o inicio do loop
         }
@@ -190,48 +195,55 @@ void login() {
         //menu de opcoes
         while (1) {
             
-            printf("\n%s - Menu de Opcoes\n", restaurantes[i].nome);
-            printf("1 - Renomear\n");
-            printf("2 - Deletar\n");
-            printf("3 - Mostrar pratos\n");
-            printf("0 - Logout\n\n");
+            printf("\n%s - Menu de Opcoes\n", restaurantes[restIndex].nome);
+            printf(" 1 - Renomear\n");
+            printf(" 2 - Deletar\n");
+            printf(" 3 - Mostrar Pratos\n");
+            printf("-1 - Logout\n\n");
+
             scanf("%d", &opcao);
 
-            if (opcao == 0) { //na etapa 2, a alteração deve ser feita aqui
+            if (opcao == 0) {
                 break; // Logout 
             } else if (opcao == 1) { //renomear
-                renomearRestaurante(&restaurantes[i]);
 
+                renomearRestaurante(&restaurantes[restIndex]);
 
             }else if(opcao==2){ // deletar restaurante
-                //restaurantes[index_restaurante].codigo = -1;
                 
-                //Deletar o nome do restaurante
-                //restaurantes[index_restaurante].nome[0]= '\0';
-
-                Restaurante lastRest = restaurantes[numRests-1];
-                restaurantes[numRests-1] = restaurantes[index_restaurante];
-                restaurantes[index_restaurante] = lastRest;
-
-                numRests--;
-                restaurantes = (Restaurante*)realloc(restaurantes,numRests*sizeof(Restaurante));
-
-                saveRests();
-
-                printf("\nRestaurante deletado com sucesso.\n");
+                deleteRest(restIndex);
 
                 goto voltaraomenu;
 
             }else if (opcao==3){
-                mostrarPratos(restaurantes[i].codigo);
-                printf("0 - Voltar ao menu anterior\n\n");
+
+                int readPratosStatusCode = readPratos();
+
+                if(readPratosStatusCode == -1) printf("\nAinda Nao Existem Pratos!\n");
+                if(readPratosStatusCode <= 0) continue;
+
+                mostrarPratos(restaurantes[restIndex].codigo);
+                printf(" 0 - Cadastrar Novo Prato\n");
+                printf("-1 - Voltar ao menu anterior\n\n");
                 scanf("%d", &pratoEscolhido);
 
                 //Ver detalhes de cada prato ou ir para o menu anterior
-                if (pratoEscolhido==0){
+                if (pratoEscolhido == -1){
                     continue; //voltar ao menu anterior
-                }else if (pratoEscolhido != 0){
-                    int index_prato = (index_restaurante*4) + pratoEscolhido -1;
+                }else if (pratoEscolhido == 0) {
+
+                    createNewPrato(restIndex);
+
+                } else {
+                    readPratos();
+                    int index_prato = findIndexOfPratoByItsCodeAndRestCode(pratoEscolhido, restaurantes[restIndex].codigo);
+
+                    if(index_prato == -1) {
+                        printf("\nPrato nao encontrado!\n");
+                        continue;
+                    }
+
+
                     printf("\n%s - Menu de Opcoes\n", pratos[index_prato].descricao);
                     printf("1 - Renomear\n");
                     printf("2 - Alterar preco\n");
@@ -249,12 +261,12 @@ void login() {
                         scanf("%f", &novo_preco);
                         index_restaurante = findIndexOfRestByCode(codigoRestaurante);
                         pratos[index_prato].preco = novo_preco;
-                        //dentro do colchetes tem que somar o numero do restaurante por conta da posição do prato no vetor
+                        
+                        savePratos();
                     } else if(aux==3){ //deletar
-                        pratos[index_prato].codigoPrato = -1;
-                        pratos[index_prato].codigoRest = -1;
-                        pratos[index_prato].preco = -1;
-                        pratos[index_prato].descricao[0]= '\0';
+
+                        deletePrato(index_prato);
+
                     }    
 
                 }
@@ -315,6 +327,27 @@ int findIndexOfRestByCode(int codigoRestaurante) {
     return -1;
 }
 
+int findIndexOfPratoByItsCodeAndRestCode(int pratoCode, int restCode) {
+    for(int i = 0; i < numPratos; i++) {
+        if(pratos[i].codigoPrato == pratoCode && pratos[i].codigoRest == restCode)
+            return i;
+    }
+    return -1;
+}
+
+int deleteRest(int restIndex) {
+    Restaurante lastRest = restaurantes[numRests-1];
+    restaurantes[numRests-1] = restaurantes[restIndex];
+    restaurantes[restIndex] = lastRest;
+
+    numRests--;
+    restaurantes = (Restaurante*)realloc(restaurantes,numRests*sizeof(Restaurante));
+
+    saveRests();
+
+    printf("\nRestaurante deletado com sucesso.\n");
+}
+
 int createNewRest() {
 
     int newRestCode = 1;
@@ -344,8 +377,6 @@ int createNewRest() {
 
     restaurantes[numRests-1].codigo = newRestCode;
 
-    char newRestName[MAX_STRING_LENGTH];
-
     printf("\nCriar novo restaurante - Digite o nome do novo restaurante:\n");
 
     fgets(restaurantes[numRests-1].nome, MAX_STRING_LENGTH, stdin);
@@ -361,8 +392,7 @@ int saveRests() {
     fprintf(fRests, "%i", numRests);
 
     for(int i = 0; i < numRests; i++) {
-        if(restaurantes[i].codigo != -1)
-            fprintf(fRests, "\n%i %s", restaurantes[i].codigo, restaurantes[i].nome);
+        fprintf(fRests, "\n%i %s", restaurantes[i].codigo, restaurantes[i].nome);
     }
 
     fclose(fRests);
@@ -376,9 +406,7 @@ int readRests() {
         free(restaurantes);
     }
 
-    FILE* fRests;
-
-    fRests = fopen(restsTxt, "r");
+    FILE* fRests = fopen(restsTxt, "r");
 
     fscanf(fRests, "%i", &numRests);
 
@@ -386,7 +414,6 @@ int readRests() {
         return -1;
     }
     
-
     restaurantes = (Restaurante*)malloc(numRests*sizeof(Restaurante));
 
     if(restaurantes == NULL) {
@@ -400,7 +427,7 @@ int readRests() {
 
         if(i >= numRests) {
             fclose(fRests);
-            printf("Erro na leitura dos dados do arquivo \"restaurante.txt\"!\n\n");
+            printf("Erro na leitura dos dados do arquivo \"%s\"!\n\n", restsTxt);
             return 0;
         }
 
@@ -414,6 +441,138 @@ int readRests() {
     }
 
     fclose(fRests);
+
+    return 1;
+}
+
+int deletePrato(int pratoIndex) {
+    Prato lastPrato = pratos[numPratos-1];
+    pratos[numPratos-1] = pratos[pratoIndex];
+    pratos[pratoIndex] = lastPrato;
+
+    numPratos--;
+    pratos = (Prato*)realloc(pratos,numPratos*sizeof(Prato));
+
+    savePratos();
+
+    printf("\nPrato deletado com sucesso.\n");
+}
+
+int createNewPrato(int restIndex) {
+
+    int newPratoCode = 1;
+
+    if(numPratos > 0) {
+        int biggestPratoCode_i = 0;
+
+        for(int i = 1; i < numPratos; i++) {
+            if(!pratos[i].codigoRest == restaurantes[restIndex].codigo)
+                continue;
+            if(pratos[i].codigoPrato > pratos[biggestPratoCode_i].codigoPrato)
+                biggestPratoCode_i = i;
+        }
+
+        newPratoCode = pratos[biggestPratoCode_i].codigoPrato + 1;
+    }
+
+    numPratos++;
+
+    if(numPratos > 1) 
+        pratos = (Prato*)realloc(pratos, numPratos*sizeof(Prato));
+    else
+        pratos = (Prato*)malloc(sizeof(Prato));
+
+    if(pratos == NULL) {
+        allocerror();
+        return 0;
+    }
+
+    pratos[numPratos-1].codigoPrato = newPratoCode;
+    pratos[numPratos-1].codigoRest = restaurantes[restIndex].codigo;
+
+    printf("\n%s - Pratos - Novo prato:\n\nDigite o nome do novo prato:\n", restaurantes[restIndex].nome);
+
+    getchar();
+    fgets(pratos[numPratos-1].descricao, MAX_STRING_LENGTH, stdin);
+    removeNewLineFromStrEnd(pratos[numPratos-1].descricao);
+
+    printf("\nDigite o preco do novo prato:\n");
+    scanf("%f", &pratos[numPratos-1].preco);
+
+    return savePratos();
+
+}
+
+int savePratos() {
+    FILE* fPratos = fopen(pratosTxt, "w");
+
+    fprintf(fPratos, "%i", numPratos);
+
+    for(int i = 0; i < numPratos; i++) {
+        fprintf(fPratos, "\n%i %i %s %g", pratos[i].codigoPrato, pratos[i].codigoRest, pratos[i].descricao, pratos[i].preco);
+    }
+
+    fclose(fPratos);
+
+    return 1;
+}
+
+int readPratos() {
+
+    if(pratos != NULL) {
+        free(pratos);
+    }
+
+    FILE* fPratos = fopen(pratosTxt, "r");
+
+    fscanf(fPratos, "%i", &numPratos);
+
+    if(numPratos == 0) {
+        return -1;
+    }
+
+    pratos = (Prato*)malloc(numPratos*sizeof(Prato));
+
+    if(pratos == NULL) {
+        allocerror();
+        return 0;
+    }
+
+    int i = 0;
+
+    while(!feof(fPratos)) {
+
+        if(i >= numPratos) {
+            fclose(fPratos);
+            printf("Erro na leitura dos dados do arquivo \"%s\"!\n\n", pratosTxt);
+            return 0;
+        }
+
+        fscanf(fPratos, "%i", &pratos[i].codigoPrato);
+        fscanf(fPratos, "%i", &pratos[i].codigoRest);
+
+        fgetc(fPratos);
+        fgets(pratos[i].descricao, MAX_STRING_LENGTH, fPratos);
+        
+        /*Pegando a última ocorrência do
+          caractere de espaço*/
+
+        char* lastSpaceIndex = strrchr(pratos[i].descricao, ' ');
+            
+        /*Limitando a descrição até o ultimo caractere de espaço.
+          Depois dele, é o preço do prato*/
+
+        *lastSpaceIndex = '\0';
+
+        char precoStr[16];
+        strcpy(precoStr, lastSpaceIndex+1);
+
+        sscanf(precoStr, "%f", &pratos[i].preco);
+
+        i++;
+    }
+
+    fclose(fPratos);
 
     return 1;
 }
